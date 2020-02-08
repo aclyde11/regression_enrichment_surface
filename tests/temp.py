e7c304d89fee437bc1367a9ac12e3f33f3a4d9be
Xiaotian Duan <xduan7@gmail.com>
2020-02-08 14:03:36 -0600

    implemented the unit test for RES grid computation

--- tests/temp.py
+++
@@ -1,7 +0,0 @@
-"""
-    File Name:          regression_enrichment_surface/**/temp.py
-    Author:             Xiaotian Duan (xduan7)
-    Email:              xduan7@uchicago.edu
-    File Description:
-
-"""
